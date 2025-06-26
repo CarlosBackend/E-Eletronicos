f822259fb443ab633a6d55cf5d0a8045d21f16a4
Carlos Henrique <carlinhosbackend@gmail.com>
2025-06-25 21:34:59 -0300

    Primeiro commit do projeto E-Eletronicos

---
+++ src/public/shared/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-letronicos</title>
+
+    <!-- INSERINDO CDN DO FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-k6RqeWeci5ZR/Lv4MR0sA0FfDOMa1z
+    <!-- INSERINDO CSS -->
+    <link rel="stylesheet" href="../../lib/aurora.css">
+</head>
+<body>
+    <header class="header-light pd-10">
+        <div class="container">
+            <div class="box-4">
+            <a href="">
+                <h1 class="fonte42 fnc-preto-1 poppins-black">e-Compras</h1>
+            </a>
+            </div>
+            <div class="box-8">
+                <ul class = "flex justify-end">
+                    <li><a href="">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
