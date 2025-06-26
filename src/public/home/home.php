7bfa7296d5175d512abd1b4c0fabb013f5f85b49
Carlos Henrique <carlinhosbackend@gmail.com>
2025-06-26 01:17:36 -0300

    Criando a fachada do site

---
+++ src/public/home/home.php
@@ -0,0 +1,15 @@
+<?php
+require_once "../shared/header.php";?>
+
+<section class="banner mg-t-4">
+    <div class="container">
+        <div class="box-8 flex justify-center item-centro">
+            <div class="box-6 mg-t-10">
+                <span class=" bg-p1-laranja fonte16 pd-l-1 block wd-50">Grandes Promoções</span>
+                <h3 class="fonte50">Venha Conferir!<br>
+                Nosso preços <br>estão incriveis
+            </h3>
+            </div>
+        </div>
+    </div>
+</section>
--- src/public/shared/header.php
+++ src/public/shared/header.php
@@ -8,24 +8,25 @@
     <!-- INSERINDO CDN DO FONT AWESOME -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- INSERINDO CSS -->
-    <link rel="stylesheet" href="aurora.css">
-    <link rel="stylesheet" href="site.css">
+    <link rel="stylesheet" href="../../lib/aurora.css">
+    <link rel="stylesheet" href="../../lib/site.css">
 </head>
 <body>
-    <header class="header-light pd-10">
+    <header class="header-light">
         <div class="container">
             <div class="box-4">
-            <a href="">
-                <h1 class="fonte42 fnc-preto-1 poppins-black">e-Compras</h1>
-            </a>
+                <a href="">
+                    <h1 class = "fonte42 fnc-preto-1 poppins-black">e-Compras</h1>
+                </a>
             </div>
             <div class="box-8">
                 <ul class = "flex justify-end pd-t-1">
-                    <li><a href="" class ="flex justify-end item-centro mg-l-1">
-                        <i class="fa-solid fa-cart-shopping fonte24 fnc-preto-1"></i> 
-                        <span class ="balao flex justifay-center item-centro poopins-black fnc-branco"> 0</span>
-                    </a>
-                </li>
+                    <li>
+                        <a href="" class="flex justify-center item-centro mg-l-1">
+                            <i class="fa-solid fa-cart-shopping fonte24 fnc-preto-1"></i>
+                            <span class = "balao flex justify-center item-centro poppins-black fnc-branco"> 0 </span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
